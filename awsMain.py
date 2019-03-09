903d46de555c15d66a22ce65de78f5f1602e63d5
dataatsky <victorcardocasa@hotmail.com>
2019-03-09 15:14:20 +0100

    parsing

--- awsMain.py
+++ awsMain.py
@@ -1,15 +1,15 @@
 
 import argparse
 import testS3 as s3aws
-import sys
 import os.path
-import boto3
 
-parser = argparse.ArgumentParser()
-parser.add_argument('buckets',help='file text with bucket names')
+parser = argparse.ArgumentParser(description='Aws Scanner')
+
 parser.add_argument('-l', '--list', required=False, dest='list', action='store_true',
-                    help='Save bucket file listing to local file: ./list-buckets/${bucket}.txt')
-client = boto3.client('s3')
+                    help='Name of text file containing buckets to check')
+parser.add_argument('-s' '--save', required=False, dest='save')
+parser.add_argument('buckets', help='Name of the bucket')
+
 args = parser.parse_args()
 
 
@@ -18,6 +18,6 @@ if os.path.isfile(args.buckets):
     with open(args.buckets, 'r') as f:
         for line in f:
             line = line.rstrip()
-            s3aws.check_bucket(line,args.list)
+            s3aws.check_bucket(line,args.list,args.save)
 else:
-    s3aws.check_bucket(args.buckets,args.list)
+    s3aws.check_bucket(args.buckets,args.list,args.save)
